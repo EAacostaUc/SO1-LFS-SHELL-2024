212838b1cafcbab4c04189dcac4609b63511a509
Eacosta <root@localhost.localdomain>
2024-12-05 17:49:07 -0300

    contiene prototipos de funciones

---
+++ prototipos.h
@@ -0,0 +1,8 @@
+#ifndef PROTOTIPOS_H
+#define PROTOTIPOS_H
+
+// Declaraciones de funciones
+int es_comando_prohibido(const char *comando);
+void ejecutar_comando(const char *comando);
+
+#endif // PROTOTIPOS_H
