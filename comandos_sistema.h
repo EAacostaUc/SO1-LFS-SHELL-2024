5318f3f79709c8e0599ee101ca03491b1d0aba9e
Eacosta <root@localhost.localdomain>
2024-12-04 23:10:39 -0300

    Nuevo agregado

---
+++ comandos_sistema.h
@@ -0,0 +1,8 @@
+#ifndef COMANDOS_SISTEMA_H
+#define COMANDOS_SISTEMA_H
+
+// Declaraciones de funciones
+int es_comando_prohibido(const char *comando);
+void ejecutar_comando(const char *comando);
+
+#endif // COMANDOS_SISTEMA_H
