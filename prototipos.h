d49100e7e1bf51626a74040e41428aa9068e6e91
Eacosta <root@localhost.localdomain>
2024-12-14 23:39:53 -0300

    se agrego nueva declaracion

--- prototipos.h
+++ prototipos.h
@@ -5,6 +5,7 @@
 int es_comando_prohibido(const char *comando);
 void ejecutar_comando(const char *comando);
 
-void registrar_error(const char *mensaje);
+void registrar_error(const char *mensaje);   // Registra los errores en el sistema que se le presentan al usuario
+void registrar_movimientos(const char *comando); // Registra comandos con un timestamp en el archivo log
 
 #endif // PROTOTIPOS_H
