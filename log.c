071a0cfb55401b6f2e5b92bc59f8cac2920cea74
Eacosta <root@localhost.localdomain>
2024-12-14 23:41:20 -0300

    se agrego nueva funcion 'registrar_movimientos'

--- log.c
+++ log.c
@@ -6,7 +6,8 @@
 #include <sys/stat.h>
 #include <errno.h>
 
-#define LOG_PATH "/var/log/shell/sistema_error.log"
+#define ERRORES_LOG_PATH "/var/log/shell/sistema_error.log"
+#define MOVIMIENTOS_LOG_PATH "/var/log/shell/shell_movimientos.log"
 
 // Función para registrar un error en el archivo de log
 void registrar_error(const char *mensaje) {
@@ -18,14 +19,14 @@ void registrar_error(const char *mensaje) {
     }
 
     // Abrir el archivo de log en modo de adición
-    FILE *log_file = fopen(LOG_PATH, "a");
+    FILE *log_file = fopen(ERRORES_LOG_PATH, "a");
     if (log_file == NULL) {
-        printf("Error al abrir el archivo de log '%s': %s\n", LOG_PATH, strerror(errno));
+        printf("Error al abrir el archivo de log '%s': %s\n", ERRORES_LOG_PATH, strerror(errno));
         return;
     }
 
     // Cambiar los permisos del archivo a 777
-    chmod(LOG_PATH, 0777);
+    chmod(ERRORES_LOG_PATH, 0777);
 
     // Obtener el timestamp actual
     time_t t = time(NULL);
@@ -38,4 +39,42 @@ void registrar_error(const char *mensaje) {
 
     // Cerrar el archivo
     fclose(log_file);
+}
+
+
+
+
+
+// Función para registrar movimientos realizados por el usuario
+void registrar_movimientos(const char *comando) {
+    FILE *log;
+    char timestamp[20];  // Espacio para el timestamp
+    char mensaje[512];   // Espacio para el mensaje formateado
+
+    // Crear el directorio /var/log/shell si no existe
+    struct stat st;
+    if (stat("/var/log/shell", &st) == -1) {
+        if (mkdir("/var/log/shell", 0777) != 0) {  // para que cualquier usuario pueda usarlo
+            printf("Error al crear el directorio '/var/log/shell': %s\n", strerror(errno));
+            return;
+        }
+    }
+
+    // Obtener el timestamp actual
+    time_t t = time(NULL);
+    struct tm *tm_info = localtime(&t);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_info);
+
+    // Formatear el mensaje
+    snprintf(mensaje, sizeof(mensaje), "%s - %s\n", timestamp, comando);
+
+    // Abrir el archivo de log en modo de añadir (append)
+    log = fopen(MOVIMIENTOS_LOG_PATH, "a");
+    if (log != NULL) {
+        // Escribir el timestamp y el comando en el archivo
+        fprintf(log, "%s", mensaje);
+        fclose(log);
+    } else {
+        printf("Error al abrir el archivo de log '%s': %s\n", MOVIMIENTOS_LOG_PATH, strerror(errno));
+    }
 }
