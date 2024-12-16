231c3e94a486333e459e7cd99a70a8d7dfa72126
Eacosta <root@localhost.localdomain>
2024-12-16 13:57:27 -0300

    Se agregos nuevas declaraciones

--- prototipos.h
+++ prototipos.h
@@ -1,5 +1,8 @@
+#include <stdio.h>
+#include <stddef.h>
 #ifndef PROTOTIPOS_H
 #define PROTOTIPOS_H
+
 
 // Declaraciones de funciones
 int es_comando_prohibido(const char *comando);
@@ -7,5 +10,10 @@ void ejecutar_comando(const char *comando);
 
 void registrar_error(const char *mensaje);   // Registra los errores en el sistema que se le presentan al usuario
 void registrar_movimientos(const char *comando); // Registra comandos con un timestamp en el archivo log
+
+void obtener_timestamp(char *buffer, size_t buffer_size);
+void obtener_ip_actual(char *ip_buffer, size_t buffer_size);
+void registrar_sesion(const char *usuario, const char *accion, const char *ip_actual, const char *horario_actual);
+void validar_inicio_sesion(const char *usuario, const char *ip_actual, const char *horario_actual, FILE *log_file);
 
 #endif // PROTOTIPOS_H
